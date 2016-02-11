01158a8bdbd6fd90cf815e36d6f9f0ca1cb40387
Kreloc <kingpincat@hotmail.com>
2016-02-11 14:22:30 -0900

    Add Client Task functions
    Added a function to find Client tasks, Get-ePoClientTask, and a function
    to start client tasks, Start-ePoClientTask

---
+++ Get-ePoClientTask.ps1
@@ -0,0 +1,58 @@
+﻿Function Get-ePoClientTask 
+{
+	<#	
+		.SYNOPSIS
+			Gets client task information based on filter parameter.
+		
+		.DESCRIPTION
+			Gets client task information based on filter parameter. Uses the clienttask.find api command
+		
+		.PARAMETER Filter
+			The search string to use for finding the client task.
+		
+		.EXAMPLE
+			Get-ePoClientTask -Filter "Full scan - Normal Priority"
+			
+			Returns information about the client task with Full scan - Normal Priority in the task name.
+			
+		.EXAMPLE
+			Get-ePoClientTask -Filter "scan"
+		
+			Returns information about client tasks with scan in the task name.
+
+        .EXAMPLE
+            $UpdateClientTasks = Get-ePoClientTask -Filter "update"
+            $DatUpdateTask = $UpdateClientTasks | Where {$_.TaskName -like "DAT Update"}
+
+            $DatUpdateTask | Start-ePoClientTask -ComputerName "NEEDSDAT-UPDT"
+
+            First gets information about clients tasks with update in the task name and stores it in a variable.
+            Then filters that variable to Where-Object, for tasks with a name like DAT Update and stores it in a variable.
+            Finally, it pipes that variable to Start-ePoClientTask, starting the Dat update client task on the computer named NEEDSDAT-UPDT.
+			
+	#>
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory=$True,
+		ValueFromPipeline=$True, ValueFromPipelinebyPropertyName=$true)]
+		[string]$Filter
+	)
+	Begin{}
+	Process 
+	{
+		$results = Invoke-ePoCommand -Command "clienttask.find" -Parameters "searchText=$($Filter)"
+        $ClientTasks = ForEach($result in $results.result.list.element.objectTask)
+        {
+            $props = @{TaskId=$result.objectId
+                       TaskName=$result.objectName
+                       ProductId=$result.productId
+                       typeId=$result.typeId
+                       typeName=$result.typeName
+            }
+        New-Object -TypeName psobject -Property $props
+        }
+        $ClientTasks
+	}
+	End{}
+}
---
+++ Start-ePoClientTask.ps1
@@ -0,0 +1,50 @@
+﻿Function Start-ePoClientTask 
+{
+	<#	
+		.SYNOPSIS
+			Starts the specified client task on specified target system.
+		
+		.DESCRIPTION
+			Starts the specified client task on specified target system. Requires the productId and taskId of the clienttask to run. Uses the
+            clienttask.run api command.
+		
+		.PARAMETER ComputerName
+			The name of the computer to start the client task on.
+		
+		.EXAMPLE
+			Start-ePoClientTask -ComputerName "THATPC" -ProductId "VIRUSCAN8800" -TaskId "111"
+			
+			Starts a client task for a Full Scan - Normal priority on the computer named "THATPC"
+			
+		.EXAMPLE
+			Get-ePoClientTask -Filter "Full scan - Normal Priority" | Start-ePoClientTask -ComputerName "THATPC"
+		
+			First uses the Get-ePoClientTask function to find the correct client task and then pipes the TaskId and ProductId to 
+            the Start-ePoClientTask function.	
+			
+	#>
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory=$True,
+		ValueFromPipeline=$True, ValueFromPipelinebyPropertyName=$true)]
+		[string]$ComputerName,
+        [Parameter(Mandatory=$True,
+		ValueFromPipelinebyPropertyName=$true)]
+		$TaskId,
+        [Parameter(Mandatory=$True,
+		ValueFromPipelinebyPropertyName=$true)]
+		$ProductId
+        	
+	)
+	Begin{}
+	Process 
+	{
+        $results = Invoke-ePoCommand -Command "clienttask.run" -Parameters "names=$($ComputerName)&productId=$($ProductId)&taskId=$($TaskId)"
+        $props = @{ResultStatus=$results.result
+                    ComputerName=$ComputerName
+                    }
+        New-Object -TypeName psobject -Property $props
+	}
+	End{}
+}
